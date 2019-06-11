9ee3859131d9c971141d5b4158a8283524fc82d9
Victor Rono <v.rono@icloud.com>
2019-06-11 16:45:12 -0500

    array created

---
+++ SampleAccord.Net/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAccord.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] inputs =
+            {
+                new double[] {0, 0},
+                new double[] {0.25, 0.25},
+                new double[] {0.5, 0.5},
+                new double[] {1, 1},
+            };
+            
+
+            Console.ReadKey();
+        }
+    }
+}
